29a3ecd713897f692c0b9edc8eef0c668b9b9cc3
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-09-22 12:22:35 +0330

    Add preFilter and PostFilter

---
+++ apigateway/src/main/kotlin/com/github/ahmadahghazadeh/apigateway/MainPostFilter.kt
@@ -0,0 +1,25 @@
+package com.github.ahmadahghazadeh.apigateway
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.filter.GlobalFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class MainPostFilter:GlobalFilter {
+    val logger: Logger = LoggerFactory.getLogger(MainPostFilter::class.java)
+
+    override fun filter(exchange: ServerWebExchange?, chain: GatewayFilterChain): Mono<Void> {
+        return chain.filter(exchange).then(Mono.fromRunnable {
+            logger.info(
+                "My last post filter is executed"
+            )
+        })
+    }
+    fun getOrder(): Int {
+        return 0
+    }
+}
