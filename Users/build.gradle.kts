666a00021480b561004ae9fe84ba3a3fead5043c
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-09-24 23:56:16 +0330

    Add ApiConfigServer

--- Users/build.gradle.kts
+++ Users/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
